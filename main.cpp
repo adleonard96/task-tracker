9af841b2d280179b43c337a4af83464f2f011543
adleonard96 <alexanderleonard27@gmail.com>
2024-08-27 18:34:59 -0400

    add check for input args

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <iostream>
 #include <string>
 #include <fstream>
+#include "Task.h"
 
 using namespace std;
 
@@ -10,9 +12,12 @@ const int INPUT_INDEX = 1;
 int main(int argc, char* argv[])
 {
 
-    // if(argc <= 1){
-    //     return 1;
-    // }
+    const string NO_PARAMETERS_PROVIDED_VALUE = "2>CON";
+    string arg1 = argv[INPUT_INDEX];
+    if(arg1 == NO_PARAMETERS_PROVIDED_VALUE){
+        std::cout << "input params where not provided";
+        return 1;
+    }
 
     FILE* fileExist = fopen("tasks.json", "r");
     if(!fileExist){
@@ -20,11 +25,12 @@ int main(int argc, char* argv[])
         fputs("{\"tasks\":[]}", taskList);
         fclose(taskList);
     }
+    fclose(fileExist);
 
     ifstream taskListInput("tasks.json");
     string line;
     while(getline(taskListInput, line)){
-        printf("%s", line);
+        printf("%s", &line);
     }
 
     printf("program input is: %s", argv[0]);
