64b72f748ab63f22c9b826295f1ed2509cb354a2
adleonard96 <alexanderleonard27@gmail.com>
2024-09-02 21:46:57 -0400

    started building out json parser

--- JsonParser.cpp
+++ JsonParser.cpp
@@ -27,7 +27,24 @@ class JsonParser {
 
             while (getline(tasksJson, line))
             {
+                size_t firstValue = line.find_first_of("\"") + 1;
                 printf("%s", line.c_str());
+                
+                string attribute;
+                printf("\n");
+                while(line[firstValue] != '"' && line[firstValue] != '\n'){
+                    string character {line[firstValue]};
+                    attribute.append(character);
+                    firstValue++;
+                }
+
+                while (line[firstValue] != '\n')
+                {
+                    if(line[firstValue] == '['){
+                        
+                    }
+                }
+                
             }
             
             return tasks;
